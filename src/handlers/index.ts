057988dcfb40a3d08dc5506055fac88ee2af4bf9
kpzaolod6000 <kpzaolod6000gmail.com>
2025-10-10 01:08:36 -0500

    feat: validator express and apply slug

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,8 +1,15 @@
 import type { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import slug from "slug";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
+
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
     const { email } = req.body;
 
@@ -12,8 +19,17 @@ export const createAccount = async (req: Request, res: Response) => {
         return res.status(409).json({ error: error.message });
     }
 
+    const handle = slug(req.body.handle);
+    const handleExists = await User.findOne({ handle });
+    if (handleExists) {
+        const error = new Error('Handle already exists');
+        return res.status(409).json({ error: error.message });
+    }
+
     const user = new User(req.body);
     user.password = await hashPassword(user.password);
+    user.handle = handle;
+
     await user.save();
     res.status(201).send(user);
 }
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,25 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { createAccount } from "./handlers";
 
 const router = Router();
 
 /** Authentications and Register */
-router.post("/auth/register", createAccount);
+router.post(
+  "/auth/register",
+  body("handle")
+    .notEmpty()
+    .withMessage("Handle is required"),
+  body("name")
+    .notEmpty()
+    .withMessage("Name is required"),
+  body("email")
+    .isEmail()
+    .withMessage("Email is invalid"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
+  createAccount
+);
 
 export default router;
